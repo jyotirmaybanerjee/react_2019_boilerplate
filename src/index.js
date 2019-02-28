aab2b5203903f6b2e581f779d4481f36bb4f8222
Jyotirmay Banerjee <jyotirmay.banerjee@hotmail.com>
2019-02-28 14:55:46 -0800

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import App from "./App.js";
+import store from './store';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/:filter?" component={App} />
+    </BrowserRouter>
+  </Provider>, 
+  document.getElementById('root')
+);
