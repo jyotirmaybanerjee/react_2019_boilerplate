7b534d07aa42f53aefcfd673d3d0e62abaceccc9
Jyotirmay Banerjee <jyotirmay.banerjee@hotmail.com>
2019-02-28 16:45:01 -0800

    Use hash router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import App from "./App.js";
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/:filter?" component={App} />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>, 
   document.getElementById('root')
 );
